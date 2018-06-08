add0952313294c09ea97d4da39f528066fcdca96
Andreas Daskalopoulos <Andreas.Daskalopoulos@citrix.com>
2018-06-08 10:25:56 +0300

    Add functionality

---
+++ ui.js
@@ -0,0 +1,11 @@
+class UI {
+    showWeather(data) {
+        document.getElementById('city-name').textContent = data.city_name;
+        document.getElementById('description').textContent = data.weather.description;
+        document.getElementById('temperature').textContent = `${data.temp} C`;
+        document.getElementById('wind').textContent = `Wind: From the ${data.wind_cdir} at ${data.wind_spd} m/s`
+        document.getElementById('feels-like').textContent = `Feels like: ${data.app_temp} C`;
+        document.getElementById('dewpoint').textContent = `Dew point: ${data.dewpt} C`;
+        document.getElementById('humidity').textContent = `Relative Humidity: ${data.rh} %`;
+    }
+}
--- weather.js
+++ weather.js
@@ -1,12 +1,20 @@
 class Weather {
 
-    constructor() {
+    constructor(city) {
         this.apiKey = '849099f4d39f43deaa3dbec6290923dc';
+        this.city = 'Athens';
+        if (city) {
+            this.city = city;
+        }
     }
 
-    async getWeather(city, countryCode) {
-        const weatherResponse = await fetch(`https://api.weatherbit.io/v2.0/current?city=Raleigh,NC&key=${this.apiKey}`);
-        const weather = await weatherResponse.json();
-        return weather
-      }
+    async getWeather() {
+        const weatherResponse = await fetch(`https://api.weatherbit.io/v2.0/current?city=${this.city}&key=${this.apiKey}`);
+        const response = await weatherResponse.json();
+        return response;
+    }
+
+    changeLocation(city) {
+        this.city = city;
+    }
 }
